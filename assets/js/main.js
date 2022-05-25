d40cdfeaeaf0168ade8ac3509dcac8996a6eaddd
arifline16 <arifline16@gmail.com>
2022-05-26 02:07:26 +0600

    bitme-arif

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,11 @@
     "use strict";
     jQuery(document).ready(function() {
         $('select').niceSelect();
+
+        $(document).on('click', '.filter_charts a', function(e) {
+            e.preventDefault();
+            $(this).closest('ul').find('a').removeClass('active');
+            $(this).addClass('active');
+        })
     })
 })(jQuery);
