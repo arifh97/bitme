dfed25df935d8c9a8c234581da3c946f0065e7b1
arifline16 <arifline16@gmail.com>
2022-05-26 23:59:31 +0600

    bitme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,17 @@
-(function($) {
+(function() {
     "use strict";
+
+    // PAGE PRELOADER JS
+    $(window).on('load', function(e) {
+        // $('body').css('overflow', 'hidden');
+        setTimeout(function() {
+            $("#preloader").fadeOut(500);
+            $('body').css('overflow', 'visible');
+        }, 1500);
+    });
+
     jQuery(document).ready(function() {
+
         $('select').niceSelect();
 
         $(document).on('click', '.filter_charts a', function(e) {
@@ -9,4 +20,4 @@
             $(this).addClass('active');
         })
     })
-})(jQuery);
+})();
