c2af4cfb9fadef162ef7bfa898e6a0f2d1745528
arifline16 <arifline16@gmail.com>
2022-05-26 22:14:26 +0600

    bitme

--- assets/chart/charts.js
+++ assets/chart/charts.js
@@ -1,4 +1,8 @@
 (function() {
+
+    /**
+     * TimeLineChart
+     **/
     var options = {
         series: [{
             data: [
@@ -284,6 +288,7 @@
             id: 'area-datetime',
             type: 'area',
             toolbar: false,
+            height: 200,
             zoom: {
                 autoScaleYaxis: true
             }
@@ -344,7 +349,332 @@
             },
         }
     };
+    var timelinechart = new ApexCharts(document.querySelector("#chart-3"), options);
+    timelinechart.render();
 
-    var chart = new ApexCharts(document.querySelector("#chart-3"), options);
-    chart.render();
+    /**
+     * BarLineChart
+     **/
+    var options = {
+        series: [{
+            name: '',
+            data: [44, 55, 57, 56, 61, 58, 63, 60, 66, 68]
+        }, {
+            name: '',
+            data: [76, 85, 101, 98, 87, 105, 91, 114, 94, 102]
+        }, {
+            name: '',
+            data: [35, 41, 36, 26, 45, 48, 52, 53, 41, 55]
+        }],
+        chart: {
+            type: 'bar',
+            height: 200,
+            toolbar: false,
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+
+        xaxis: {
+            categories: ['03:00AM', '06:00AM', '09:00AM', '11:00AM', '01:00AM', '03:00PM', '06:00PM', '08:00PM', '10:00PM'],
+
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false
+            },
+        },
+
+        yaxis: {},
+        fill: {
+            opacity: 1
+        },
+        colors: ['#00CBFF', '#48C81B', '#FFB411'],
+        grid: {
+            xaxis: {
+                lines: {
+                    show: false
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: true
+                }
+            },
+            borderColor: '#223246',
+            strokeDashArray: 7,
+        },
+    };
+    var barchart = new ApexCharts(document.querySelector("#chart-4"), options);
+    barchart.render();
+
+    /**
+     * BaseLineChart
+     **/
+    var options = {
+        series: [{
+            name: 'series1',
+            data: [31, 40, 28, 51, 42, 109, 100, ]
+        }],
+
+        chart: {
+            id: 'area-datetime',
+            height: 300,
+            type: 'area',
+            toolbar: false,
+            zoom: {
+                autoScaleYaxis: true
+            }
+        },
+        fill: {
+            type: 'gradient',
+            colors: ['rgba(0, 203, 255, 0.35)', 'rgba(0, 203, 255, 0)'],
+            gradient: {
+                inverseColors: false,
+                opacityFrom: 1,
+                opacityTo: 0,
+                stops: [70, 98, 100],
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+
+        markers: {
+            size: 0,
+            style: 'hollow',
+        },
+        stroke: {
+            curve: 'smooth',
+        },
+        xaxis: {
+            type: 'datetime',
+            min: new Date('2018-09-19T00:00:00.000Z').getTime(),
+            tickAmount: 5,
+            categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"],
+            labels: {
+                show: true,
+                datetimeFormatter: {
+                    hour: 'HH:mmTT',
+                },
+                style: {
+                    fontSize: '13px',
+                    fontWeight: 400,
+                    cssClass: 'apexcharts-xaxis-label',
+                },
+            },
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false
+            },
+
+        },
+
+        yaxis: {
+            show: false,
+        },
+        tooltip: {
+            x: {
+                format: 'dd/MM/yy HH:mm'
+            },
+        },
+        grid: {
+            xaxis: {
+                lines: {
+                    show: false
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false
+                }
+            },
+        }
+    };
+    var basechart = new ApexCharts(document.querySelector("#chart-timeline"), options);
+    basechart.render();
+    document.querySelector('#one_month')
+        .addEventListener('click', function(e) {
+            basechart.zoomX(
+                new Date('2018-09-19T00:00:00.000Z').getTime(),
+                new Date('2018-09-19T01:30:00.000Z').getTime()
+            )
+        })
+
+    document.querySelector('#six_months')
+        .addEventListener('click', function(e) {
+            basechart.zoomX(
+                new Date('2018-09-19T00:30:00.000Z').getTime(),
+                new Date('2018-09-19T03:30:00.000Z').getTime()
+            )
+        })
+
+    document.querySelector('#one_year')
+        .addEventListener('click', function(e) {
+            basechart.zoomX(
+                new Date('2018-09-19T00:30:00.000Z').getTime(),
+                new Date('2018-09-19T04:30:00.000Z').getTime()
+            )
+        })
+
+    document.querySelector('#ytd').addEventListener('click', function(e) {
+        basechart.zoomX(
+            new Date('2018-09-19T00:30:00.000Z').getTime(),
+            new Date('2018-09-19T05:30:00.000Z').getTime()
+        )
+    })
+
+    document.querySelector('#all').addEventListener('click', function(e) {
+        basechart.zoomX(
+            new Date('2018-09-19T00:30:00.000Z').getTime(),
+            new Date('2018-09-19T06:30:00.000Z').getTime()
+        )
+    })
+
+    /**
+     * LimitLineChart
+     **/
+    var options = {
+        series: [{
+            name: 'series1',
+            data: [31, 40, 28, 51, 42, 109, 100, ]
+        }],
+
+        chart: {
+            id: 'area-datetime',
+            height: 300,
+            type: 'area',
+            toolbar: false,
+            zoom: {
+                autoScaleYaxis: true
+            }
+        },
+        fill: {
+            type: 'gradient',
+            colors: ['rgba(0, 203, 255, 0.35)', 'rgba(0, 203, 255, 0)'],
+            gradient: {
+                inverseColors: false,
+                opacityFrom: 1,
+                opacityTo: 0,
+                stops: [70, 98, 100],
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+
+        markers: {
+            size: 0,
+            style: 'hollow',
+        },
+        stroke: {
+            curve: 'smooth',
+        },
+        xaxis: {
+            type: 'datetime',
+            min: new Date('2018-09-19T00:00:00.000Z').getTime(),
+            tickAmount: 5,
+            categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"],
+            labels: {
+                show: true,
+                datetimeFormatter: {
+                    hour: 'HH:mmTT',
+                },
+                style: {
+                    fontSize: '13px',
+                    fontWeight: 400,
+                    cssClass: 'apexcharts-xaxis-label',
+                },
+            },
+            axisBorder: {
+                show: false,
+            },
+            axisTicks: {
+                show: false
+            },
+
+        },
+
+        yaxis: {
+            show: false,
+        },
+        tooltip: {
+            x: {
+                format: 'dd/MM/yy HH:mm'
+            },
+        },
+        grid: {
+            xaxis: {
+                lines: {
+                    show: false
+                }
+            },
+            yaxis: {
+                lines: {
+                    show: false
+                }
+            },
+        }
+    };
+
+    var chartLimit = new ApexCharts(document.querySelector("#chart-timeline-limit"), options);
+    chartLimit.render();
+
+    document
+        .querySelector('#limit_one_month')
+        .addEventListener('click', function(e) {
+            chartLimit.zoomX(
+                new Date('2018-09-19T00:00:00.000Z').getTime(),
+                new Date('2018-09-19T01:30:00.000Z').getTime()
+            )
+        })
+
+    document
+        .querySelector('#limit_six_months')
+        .addEventListener('click', function(e) {
+            chartLimit.zoomX(
+                new Date('2018-09-19T00:30:00.000Z').getTime(),
+                new Date('2018-09-19T03:30:00.000Z').getTime()
+            )
+        })
+
+    document
+        .querySelector('#limit_one_year')
+        .addEventListener('click', function(e) {
+            chartLimit.zoomX(
+                new Date('2018-09-19T00:30:00.000Z').getTime(),
+                new Date('2018-09-19T04:30:00.000Z').getTime()
+            )
+        })
+
+    document.querySelector('#limit_ytd').addEventListener('click', function(e) {
+        chartLimit.zoomX(
+            new Date('2018-09-19T00:30:00.000Z').getTime(),
+            new Date('2018-09-19T05:30:00.000Z').getTime()
+        )
+    })
+
+    document.querySelector('#limit_all').addEventListener('click', function(e) {
+        chartLimit.zoomX(
+            new Date('2018-09-19T00:30:00.000Z').getTime(),
+            new Date('2018-09-19T06:30:00.000Z').getTime()
+        )
+    })
+
+
 })();
